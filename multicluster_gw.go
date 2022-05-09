5676ad25efd905efb7967a3cc84330a32d4fe58d
Itay Nakash <Itay.Nakash@ibm.com>
2022-05-09 16:58:45 +0300

    added multicluster_gw skelaton

--- multicluster_gw.go
+++ multicluster_gw.go
@@ -5,6 +5,7 @@ package multicluster_gw
 
 import (
 	"context"
+	"errors"
 
 	"github.com/coredns/coredns/plugin"
 	"github.com/coredns/coredns/plugin/metrics"
@@ -13,22 +14,41 @@ import (
 	"github.com/miekg/dns"
 )
 
+const (
+	// defaultTTL to apply to all answers.
+	// #TODO maybe add it to the config?
+	defaultTTL = 5
+)
+
+var (
+	errNoItems        = errors.New("no items found")
+	errNsNotExposed   = errors.New("namespace is not exposed")
+	errInvalidRequest = errors.New("invalid query name")
+)
+
 // Define log to be a logger with the plugin name in it. This way we can just use log.Info and
 // friends to log.
-var log = clog.NewWithPlugin("example")
+var log = clog.NewWithPlugin(pluginName)
 
-// Example is an example plugin to show how to write a plugin.
-type Example struct {
-	Next plugin.Handler
+//MultiCluster implements a plugin supporting multi-cluster DNS spec using a gateway.
+type MultiCluster struct {
+	Next  plugin.Handler
+	Zones []string
+	ttl   uint32
 }
 
-// ServeDNS implements the plugin.Handler interface. This method gets called when example is used
-// in a Server.
-func (e Example) ServeDNS(ctx context.Context, w dns.ResponseWriter, r *dns.Msg) (int, error) {
-	// This function could be simpler. I.e. just fmt.Println("example") here, but we want to show
-	// a slightly more complex example as to make this more interesting.
-	// Here we wrap the dns.ResponseWriter in a new ResponseWriter and call the next plugin, when the
-	// answer comes back, it will print "example".
+func New(zones []string) *MultiCluster {
+	m := MultiCluster{
+		Zones: zones,
+	}
+
+	m.ttl = defaultTTL
+
+	return &m
+}
+
+// ServeDNS implements the plugin.Handler interface.
+func (m MultiCluster) ServeDNS(ctx context.Context, w dns.ResponseWriter, r *dns.Msg) (int, error) {
 
 	// Debug log that we've have seen the query. This will only be shown when the debug plugin is loaded.
 	log.Debug("Received response")
@@ -40,11 +60,11 @@ func (e Example) ServeDNS(ctx context.Context, w dns.ResponseWriter, r *dns.Msg)
 	requestCount.WithLabelValues(metrics.WithServer(ctx)).Inc()
 
 	// Call next plugin (if any).
-	return plugin.NextOrFailure(e.Name(), e.Next, ctx, pw, r)
+	return plugin.NextOrFailure(m.Name(), m.Next, ctx, pw, r)
 }
 
 // Name implements the Handler interface.
-func (e Example) Name() string { return "example" }
+func (m MultiCluster) Name() string { return pluginName }
 
 // ResponsePrinter wrap a dns.ResponseWriter and will write example to standard output when WriteMsg is called.
 type ResponsePrinter struct {
@@ -58,6 +78,6 @@ func NewResponsePrinter(w dns.ResponseWriter) *ResponsePrinter {
 
 // WriteMsg calls the underlying ResponseWriter's WriteMsg method and prints "example" to standard output.
 func (r *ResponsePrinter) WriteMsg(res *dns.Msg) error {
-	log.Info("example")
+	log.Info(pluginName)
 	return r.ResponseWriter.WriteMsg(res)
 }
