e1326fc100b6e294b593e3149941cbd50139356d
Itay Nakash <Itay.Nakash@ibm.com>
2022-05-16 16:28:18 +0300

    finish mcs_gw skelaton

--- setup.go
+++ setup.go
@@ -1,4 +1,4 @@
-package multicluster_gw
+package multicluster
 
 import (
 	"net"
@@ -6,6 +6,7 @@ import (
 	"github.com/coredns/caddy"
 	"github.com/coredns/coredns/core/dnsserver"
 	"github.com/coredns/coredns/plugin"
+	"k8s.io/client-go/tools/clientcmd"
 )
 
 const pluginName = "multicluster_gw"
@@ -27,7 +28,12 @@ func setup(c *caddy.Controller) error {
 		return plugin.Error(pluginName, err)
 	}
 
-	// #TODO init the controller here
+	// ------------#TODO init the controller here------------
+	// tmp code until multiCluster will be used:
+	if multiCluster == nil {
+		print("err")
+	}
+	// -----------------------------------------------------------
 
 	// Add the Plugin to CoreDNS, so Servers can use it in their plugin chain.
 	dnsserver.GetConfig(c).AddPlugin(func(next plugin.Handler) plugin.Handler {
@@ -44,18 +50,43 @@ func ParseStanza(c *caddy.Controller) (*MultiCluster, error) {
 
 	zones := plugin.OriginsFromArgsOrServerBlock(c.RemainingArgs(), c.ServerBlockKeys)
 	multiCluster := New(zones)
-	multiCluster.Gateway_ip4 = net.IPv4(1, 2, 3, 4)
-	multiCluster.Gateway_ip6 = net.IPv4(1, 2, 3, 4) // #TODO find how to define ip6
+	multiCluster.gateway_ip4 = net.IPv4(1, 2, 3, 4)
+	multiCluster.gateway_ip6 = net.IPv4(1, 2, 3, 4).To16()
 
 	for c.NextBlock() {
 		switch c.Val() {
 		case "kubeconfig":
-			//#TODO  update here to get the kubeconfig - check if needed (think so)
-			//#TODO q  check if here you get the global var for the gateway ip
+			args := c.RemainingArgs()
+			if len(args) != 1 && len(args) != 2 {
+				return nil, c.ArgErr()
+			}
+			overrides := &clientcmd.ConfigOverrides{}
+			if len(args) == 2 {
+				overrides.CurrentContext = args[1]
+			}
+			config := clientcmd.NewNonInteractiveDeferredLoadingClientConfig(
+				&clientcmd.ClientConfigLoadingRules{ExplicitPath: args[0]},
+				overrides,
+			)
+			multiCluster.ClientConfig = config
+
 		case "fallthrough":
-			//#TODO q  check if needed
-		case "noendpoints":
-			//#TODO q check if needed
+			multiCluster.Fall.SetZonesFromArgs(c.RemainingArgs())
+
+		case "gateway_ip":
+			ip_as_string := c.RemainingArgs()[0]
+			ip_address_len := 4
+			if len(ip_as_string) != ip_address_len {
+				// #TODO check if it maybe can be 6?
+				return nil, c.ArgErr()
+			}
+
+			input_ip := []byte(ip_as_string)
+			// #TODO maybe check if it even possible to squeze the gateway to only 4 bytes? :
+			multiCluster.gateway_ip4 = net.IPv4(input_ip[0], input_ip[1], input_ip[2], input_ip[3])
+			// #TODO find if it reasnoble way to define ip6
+			multiCluster.gateway_ip6 = net.IPv4(input_ip[0], input_ip[1], input_ip[2], input_ip[3]).To16()
+
 		default:
 			return nil, c.Errf("unknown property '%s'", c.Val())
 		}
--- setup_test.go
+++ setup_test.go
@@ -1,4 +1,4 @@
-package multicluster_gw
+package multicluster
 
 import (
 	"testing"
