313e434ae1b3cfa596303bd684b4cfba532de2bf
Itay nakash <itaynaka@gmail.com>
2022-06-12 18:12:27 +0300

    saved nextPlugin in setup

--- setup.go
+++ setup.go
@@ -39,6 +39,7 @@ func setup(c *caddy.Controller) error {
 
 	// Add the Plugin to CoreDNS, so Servers can use it in their plugin chain.
 	dnsserver.GetConfig(c).AddPlugin(func(next plugin.Handler) plugin.Handler {
+		multiCluster.Next = next
 		return multiCluster
 	})
 
